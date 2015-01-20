4f90a77ff97900b67807c0556475c16591d12a9e
Robciuc Ioan <robciucioan94@gmail.com>
2015-01-20 15:45:16 +0200

    Issue #46 fixed
    Issue fixed:
    
    Adauga rtl.css in main theme folder

--- functions.php
+++ functions.php
@@ -98,7 +98,7 @@ function mooveit_lite_wp_enqueue_style_movatique() {
     wp_enqueue_style( 'font-family-source-sans-pro', '//fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900,200italic,300italic,400italic,600italic,700italic,900italic' );
     wp_enqueue_style( 'font-family-roboto', '//fonts.googleapis.com/css?family=Roboto:400,300,300italic,400italic,500,500italic,700,700italic,900,900italic,100italic,100' );
     if ( is_rtl() ) {
-        wp_enqueue_style( 'rtl', get_template_directory_uri() . '/css/rtl.css', array(), '1.0' );
+        wp_enqueue_style( 'rtl', get_template_directory_uri() . '/rtl.css', array(), '1.0' );
     }
 }
 add_action( 'wp_enqueue_scripts', 'mooveit_lite_wp_enqueue_style_movatique' );
