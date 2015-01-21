166be1ac8dbd70da167e2d4ed5aa48cc659c8417
Robciuc Ioan <robciucioan94@gmail.com>
2015-01-21 21:09:28 +0200

    Issue #72 fixed
    Issue fixed:
    
    Sidebar notice: "The sidebar is not active.” - could you display this
    message only for logged in users? Users might simply not want to add any
    widgets to the sidebar, and that would leave them with this message
    visible to the visitors.

--- page-blog.php
+++ page-blog.php
@@ -13,7 +13,14 @@ get_header();
 	<div class="blog-title">
 		<h3><?php _e( 'Our New Blog', 'mooveit_lite' ); ?></h3>
 	</div><!--/.blog-title-->
-	<div class="content-left">
+	<?php
+	if ( !is_active_sidebar( 'general_sidebar' ) ) {
+		$contentleft_class = 'content-left content-left-fullwidth';
+	} else {
+		$contentleft_class = 'content-left';
+	}
+	?>
+	<div class="<?php echo $contentleft_class; ?>">
 		<?php
 
 			$args = array (
--- sidebar.php
+++ sidebar.php
@@ -5,12 +5,12 @@
  * 	@package ThemeIsle
  */
 ?>
-<div class="sidebar">
-	<?php
-		if ( is_active_sidebar( 'general_sidebar' ) ) {
-			dynamic_sidebar( 'general_sidebar' );
-		} else {
-			echo __( 'The sidebar is not active.', 'mooveit_lite' );
-		}
-	?>
-</div><!--/.sidebar-->
+<?php
+if ( is_active_sidebar( 'general_sidebar' ) ) {
+
+	echo '<div class="sidebar">';
+	dynamic_sidebar( 'general_sidebar' );
+	echo '</div><!--/.sidebar-->';
+
+}
+?>
