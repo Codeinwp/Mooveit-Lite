a243a5dbb7085e005060a79cb2740e4611dbc271
Robciuc Ioan <robciucioan94@gmail.com>
2015-02-04 15:34:23 +0200

    Issue #93 (prefix styles) fixed
    Please prefix custom script and stylesheet handles in
    mooveit_lite_wp_enqueue_style_movatique() and
    mooveit_lite_wp_enqueue_script_movatique(), like so:
    wp_enqueue_style( 'mooveit_lite_style', get_stylesheet_uri(), array(),
    '1.5' );
    
    @robi09 @robciucioan

--- functions.php
+++ functions.php
@@ -81,7 +81,7 @@ add_action( 'after_setup_theme', 'mooveit_lite_setup' );
 /**
  *  WP Enqueue Style
  */
-function wp_enqueue_style() {
+function mooveit_lite_wp_enqueue_style() {
     wp_enqueue_style( 'mooveit_lite_style', get_stylesheet_uri(), array(), '1.5' );
     wp_enqueue_style( 'nivo-lightbox', get_template_directory_uri() . '/css/nivo-lightbox.css', array(), '1.2.0' );
     wp_enqueue_style( 'font-family-archivo-narrow', '//fonts.googleapis.com/css?family=Archivo+Narrow:400,400italic,700,700italic' );
@@ -91,12 +91,12 @@ function wp_enqueue_style() {
         wp_enqueue_style( 'rtl', get_template_directory_uri() . '/rtl.css', array(), '1.0' );
     }
 }
-add_action( 'wp_enqueue_scripts', 'wp_enqueue_style' );
+add_action( 'wp_enqueue_scripts', 'mooveit_lite_wp_enqueue_style' );
 
 /**
  *  WP Enqueue Script
  */
-function wp_enqueue_script() {
+function mooveit_lite_wp_enqueue_script() {
     wp_enqueue_script( 'masonry' );
     wp_enqueue_script( 'jquery.matchHeight', get_template_directory_uri() . '/js/jquery.matchHeight.js', array( 'jquery' ), '0.5.2', true );
     wp_enqueue_script( 'nivo-lightbox.min', get_template_directory_uri() . '/js/nivo-lightbox.min.js', array( 'jquery' ), '1.2.0', true );
@@ -105,7 +105,7 @@ function wp_enqueue_script() {
 
     if ( is_singular() ) wp_enqueue_script( "comment-reply" );
 }
-add_action( 'wp_enqueue_scripts', 'wp_enqueue_script' );
+add_action( 'wp_enqueue_scripts', 'mooveit_lite_wp_enqueue_script' );
 
 /**
  * Load only in IE as of WP 4.1
